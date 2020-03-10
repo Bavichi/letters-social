9ae0d6f1950762733d93503cb8d04ecb7126d5a6
Bavichi <909452380@qq.com>
2020-03-10 09:04:16 +0800

    chapter-10-11-firebase

--- test/constants/initialState.test.js
+++ test/constants/initialState.test.js
@@ -10,8 +10,7 @@ describe('initialState', () => {
             commentIds: [],
             comments: {},
             pagination: {
-                first: `${process.env
-                    .ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`,
+                first: `api/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`,
                 next: null,
                 prev: null,
                 last: null
