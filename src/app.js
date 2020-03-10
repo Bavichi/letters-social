ef13c88bad35beca5c54b9d3eaf279c793fb46c5
Bavichi <909452380@qq.com>
2020-03-10 19:34:17 +0800

    chapter-4

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ class App extends Component {
                     const links = parseLinkHeader(res.headers.get('Link'));
                     this.setState(() => ({
                         posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
-                        endpoint: links.next.url.slice(19),
+                        endpoint: 'api/'+ links.next.url.slice(19),
                     }));
                 });
             })
