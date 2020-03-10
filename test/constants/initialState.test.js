2563b0d353fa38db698602bfcb70c5b1501121ba
Bavichi <909452380@qq.com>
2020-03-10 09:02:09 +0800

    chapter-10-11

--- test/constants/initialState.test.js
+++ test/constants/initialState.test.js
@@ -10,18 +10,16 @@ describe('initialState', () => {
             commentIds: [],
             comments: {},
             pagination: {
-                first: `${process.env
-                    .ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`,
+                first: `api/posts?_page=1&_sort=date&_order=DESC&_embed=comments&_expand=user&_embed=likes`,
                 next: null,
                 prev: null,
                 last: null
             },
             user: {
-                authenticated: false,
-                profilePicture: null,
-                id: null,
-                name: null,
-                token: null
+                authenticated: true,
+                id:"7d7bab7c-e98b-412e-9b20-2b6278427aa6",
+                name:"Gallandro",
+                profilePicture:"/static/assets/users/14.jpeg"
             }
         };
         expect(initialState).toEqual(expected);
