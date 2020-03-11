c79654f51307d864c63a2547567df4e8ef0997fe
Bavichi <909452380@qq.com>
2020-03-11 16:50:30 +0800

    8

--- src/pages/home.js
+++ src/pages/home.js
@@ -29,7 +29,7 @@ export class Home extends Component {
                     const links = parseLinkHeader(res.headers.get('Link'));
                     this.setState(() => ({
                         posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
-                        endpoint: links.next.url.slice(19)
+                        endpoint: 'api/'+ links.next.url.slice(19)
                     }));
                 });
             })
